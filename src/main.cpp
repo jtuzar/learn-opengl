a81a0974bfd0aa466acf1e0e805270403a13c4d2
Jakub Tuzar <jakub@tuzar.me>
2025-12-13 22:21:29 +0100

    horizontal offset via uniform

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
+        shader.setFloat("offset", 0.5f);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
